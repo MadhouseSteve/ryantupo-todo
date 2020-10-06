510f6bca9c190d8d29409ded80fc56ea75002d71
Stevan Goode <steve@madhouseminers.com>
2020-10-06 21:21:13 +0100

    Makr pretty, Save data, Remove item, Mark done

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,60 @@
 interface ToDoItem {
-    name: string;
+  name: string;
+  complete: boolean;
+  children?: ToDoItem[];
 }
 
-let toDoList: ToDoItem[] = [];
-
-toDoList.push({name:"blah blah"});
-
-toDoList.push({name:"number two!"});
-
-toDoList.push({name:"threeee means cheese"});
+let toDoList: ToDoItem[] = JSON.parse(localStorage.getItem("todoList") ?? "[]");
 
 function markDone(event: MouseEvent) {
-    if ((event.target as HTMLElement).tagName !== "INPUT") {
-        return;
-    }    
-    if ((event.target as HTMLInputElement).type !== "checkbox"){
-        return;
-    }
-    console.log((event.target as HTMLElement).dataset);
+  if ((event.target as HTMLElement).tagName !== "INPUT") {
+    return;
+  }
+  if ((event.target as HTMLInputElement).type !== "checkbox") {
+    return;
+  }
+
+  toDoList[
+    parseInt((event.target as HTMLElement).dataset["index"])
+  ].complete = !toDoList[
+    parseInt((event.target as HTMLElement).dataset["index"])
+  ].complete;
+
+  showList();
 }
 document.onclick = markDone;
 
-function showList(){
-    let listContent = toDoList.map((toDoItem, index) => `<li> <input type="checkbox" data-index="${index}"/>${toDoItem.name}</li>`).join("");
-    document.getElementById("todolist").innerHTML = listContent;
+function showList() {
+  let listContent = toDoList
+    .map(
+      (toDoItem, index) =>
+        `<li class="list-group-item ${
+          toDoItem.complete ? "complete" : ""
+        }"> <input type="checkbox" ${
+          toDoItem.complete ? "checked" : ""
+        } data-index="${index}"/>${toDoItem.name}<button class="btn btn-dark btn-sm">+</button></li>`
+    )
+    .join("");
+  document.getElementById("todolist").innerHTML = listContent;
+  localStorage.setItem("todoList", JSON.stringify(toDoList));
 }
 
-function addToDo(){
-    
-    let boxInput = (document.getElementById("newToDo") as HTMLInputElement).value
-    if (boxInput == ""){
-        return;
-    }
-    toDoList.push({name:boxInput});
-    showList();
-    console.log("nadia and lewis sitting in a tree");
-    (document.getElementById("newToDo") as HTMLInputElement).value = "";
-};
-    
+function addToDo() {
+  let boxInput = (document.getElementById("newToDo") as HTMLInputElement).value;
+  if (boxInput == "") {
+    return;
+  }
+  toDoList.push({ name: boxInput, complete: false });
+  showList();
+  (document.getElementById("newToDo") as HTMLInputElement).value = "";
+}
+
 document.getElementById("addToDo").onclick = addToDo;
 showList();
+
+function clearCompleted() {
+  toDoList = toDoList.filter(listItem => !listItem.complete);
+  showList();
+}
+
+document.getElementById("clearCompleted").onclick = clearCompleted;
